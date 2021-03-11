b9fd33f5b33f61ea43bb1a2d1c4f1e67cc52ccb0
RosellaCoder <80446857+RosellaCoder@users.noreply.github.com>
2021-03-11 00:14:43 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,2 +1,6 @@
+function isWhitelisted(word) {
+    return new RegExp('[pP]\w*[Nn]\w*[Ss]').test(word);
+}
+
 var bad = require('bad-words/lib/lang.json').words
-module.exports = str => bad.some(word => str.includes(word))
+module.exports = str => bad.some(word => str.includes(word) && !isWhitelisted(word));
